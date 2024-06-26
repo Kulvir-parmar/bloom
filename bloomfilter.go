4342a672c50692dfe7668b27e3b09e941ccfcbb7
Kulvir Singh <kulvirs734@gmail.com>
2024-06-26 15:27:15 +0530

    ✅ weird mathematics test cases passes

--- bloomfilter.go
+++ bloomfilter.go
@@ -1,1 +1,44 @@
 package bloomfilter
+
+import (
+	"errors"
+	"math"
+)
+
+type bf struct {
+	numHashFunctions int
+	buffer           []uint64
+}
+
+func NewBloomFilter(fpRate float64, expectedInsertions int64) (*bf, error) {
+	if fpRate < 0.0 {
+		return nil, errors.New("False Positive Rate must be greater than 0")
+	}
+	if fpRate >= 1.0 {
+		return nil, errors.New("False Positive Rate must be smaller than 1")
+	}
+	if expectedInsertions <= 0 {
+		return nil, errors.New("No of insertions must be greater than 0")
+	}
+
+	numBits := numBits(fpRate, expectedInsertions)
+	numHashFunction := numHasFunctions(numBits, expectedInsertions)
+	size := uint(math.Ceil(float64(numBits) / 64.0))
+
+	return &bf{
+		numHashFunctions: numHashFunction,
+		buffer:           make([]uint64, size),
+	}, nil
+}
+
+// num of bits required for given insertions (n) and fp rate (e) are given by formula
+// numBits = Ceil((n * log(e)) / log(1 / pow(2, log(2))))
+func numBits(fpRate float64, expectedInsertions int64) int64 {
+	return int64(math.Ceil((float64(expectedInsertions) * math.Log(fpRate)) / math.Log(1/math.Pow(2, math.Log(2)))))
+}
+
+// num of hash functions recommended for given bits(m) and expectedInsertions(n) are given by formula
+// numHashFunctions = round((m / n) * log(2))
+func numHasFunctions(numBits, expectedInsertions int64) int {
+	return int(math.Round((float64(numBits) / float64(expectedInsertions)) * math.Log(2)))
+}
--- bloomfilter_test.go
+++ bloomfilter_test.go
@@ -4,5 +4,37 @@ import (
 	"testing"
 )
 
-func testA(t *testing.T) {
+func TestNumBits(t *testing.T) {
+	type tc struct {
+		numInsertions int64
+		fpRate        float64
+		numBits       int64
+	}
+
+	cases := []tc{{10000, 0.001, 143776}, {1000000, 0.01, 9585059}, {1000000, 0.5, 1442696}}
+
+	for _, c := range cases {
+		bits := numBits(c.fpRate, c.numInsertions)
+
+		if bits != c.numBits {
+			t.Errorf("Expected %d and got %d number of bits", c.numBits, bits)
+		}
+	}
+}
+
+func TestNumHashFunctions(t *testing.T) {
+	type tc struct {
+		numBits       int64
+		numInsertions int64
+		numHashes     int
+	}
+
+	cases := []tc{{9585059, 1000000, 7}, {14378, 1000, 10}, {19171, 1000, 13}}
+
+	for _, c := range cases {
+		hashes := numHasFunctions(c.numBits, c.numInsertions)
+		if hashes != c.numHashes {
+			t.Errorf("Expected %d and got %d number of hash functions", c.numHashes, hashes)
+		}
+	}
 }
